b424a460e7ee3737b4494fd182a1629ceb736b5f
Scacciotti <scacciot17@up.edu>
2016-03-14 17:35:50 -0700

    TTRState: started

---
+++ app/src/main/java/tickets/scacciot17/tickettooride/ttr/TTRState.java
@@ -0,0 +1,16 @@
+package tickets.scacciot17.tickettooride.ttr;
+
+import tickets.scacciot17.tickettooride.Game.infoMsg.GameState;
+
+/**
+ * Created by Nicka on 3/14/2016.
+ */
+public class TTRState extends GameState {
+
+
+
+
+
+
+
+}
