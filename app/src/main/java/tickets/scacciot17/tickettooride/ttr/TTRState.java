57b277798832b047a689ece754985af9ed8eae79
larsonn17 <larsonn17@up.edu>
2016-03-14 21:41:33 -0700

    2nd test commit w/changes

--- app/src/main/java/tickets/scacciot17/tickettooride/ttr/TTRState.java
+++ app/src/main/java/tickets/scacciot17/tickettooride/ttr/TTRState.java
@@ -8,6 +8,24 @@ import tickets.scacciot17.tickettooride.Game.infoMsg.GameState;
 public class TTRState extends GameState {
 
 
+    //present in slapjack; added it here
+    private static final long serialVersionUID = -8269749892027578792L;
+    //INSTANCE VARIABLES
+    //assumes deck setup as used by slapjack
+
+    //public decks
+    public Deck[] destination;
+    public Deck[] trainCards;
+    public Deck[] faceUpCards;
+    //private human/ai decks
+    //allocates for max number of computer opponents regardless
+    private Deck[] human;
+    private Deck[] computer1;
+    private Deck[] computer2;
+    private Deck[] computer3;
+    private Deck[] computer4;
+    //int used to determine which whose turn it is
+    private int playerTurn;
 
 
 }
