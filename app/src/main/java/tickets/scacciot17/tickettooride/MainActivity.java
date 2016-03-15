e6f126fb6e10ec4155c7ebfac368f2cba4dda899
Scacciotti <scacciot17@up.edu>
2016-03-14 17:09:54 -0700

    Initial commit

---
+++ app/src/main/java/tickets/scacciot17/tickettooride/MainActivity.java
@@ -0,0 +1,13 @@
+package tickets.scacciot17.tickettooride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/tickets/scacciot17/tickettooride/ttr/TicketToRideGame.java
@@ -0,0 +1,10 @@
+package tickets.scacciot17.tickettooride.ttr;
+
+import tickets.scacciot17.tickettooride.Game.Game;
+
+/**
+ * Created by Nicka on 3/14/2016.
+ */
+public interface TicketToRideGame extends Game {
+
+}
