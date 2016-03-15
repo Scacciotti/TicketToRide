920433c1371cf1c4f4c5521bf319a41cf8b61f99
schibel50 <schibel17@up.edu>
2016-03-15 15:03:40 -0700

    Initial Commit

--- app/src/main/java/tickets/scacciot17/tickettooride/ttr/TTRState.java
+++ app/src/main/java/tickets/scacciot17/tickettooride/ttr/TTRState.java
@@ -1,11 +1,14 @@
 package tickets.scacciot17.tickettooride.ttr;
 
+import tickets.scacciot17.tickettooride.Game.GamePlayer;
 import tickets.scacciot17.tickettooride.Game.infoMsg.GameState;
 
 /**
  * Created by Nicka on 3/14/2016.
  */
 public class TTRState extends GameState {
+    GamePlayer currPlayer;
+
 
 
 
