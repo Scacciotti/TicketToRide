150fc5ccec3fff087c36ff3de054c538f4a3f4f0
schibel50 <schibel17@up.edu>
2016-03-15 20:01:36 -0700

    Destination Cards and Train Cards finished

---
+++ app/src/main/java/tickets/scacciot17/tickettooride/Game/card/DestCards.java
@@ -0,0 +1,24 @@
+package tickets.scacciot17.tickettooride.Game.card;
+
+/**
+ * Created by Parker on 3/15/2016.
+ */
+public class DestCards extends Cards{
+    private final String[] destNames = {"Denver", "El Paso", "Kansas City",
+            "Houston", "New York", "Atlanta", "Chicago", "New Orleans", "Calgary",
+            "Salt Lake City", "Helena", "Los Angeles", "Duluth", "Sault Ste Marie",
+            "Nashville", "Montreal", "Oklahoma City", "Seattle", "Santa Fe",
+            "Toronto", "Miami", "Portland", "Phoenix", "Dallas", "New York City",
+            "Pittsburgh", "Winnipeg", "Little Rock", "Boston", "Vancouver",
+            "San Francisco"};
+
+    private String city1;
+    private String city2;
+    private int score;
+
+    public DestCards(int dest1, int dest2, int point){
+        city1 = destNames[dest1];
+        city2 = destNames[dest2];
+        score = point;
+    }
+}
